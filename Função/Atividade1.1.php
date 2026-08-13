5aec8b574f17aa5c8a5254849969a9ab23bdcc4a
leodm5674 <98219987+leodm5674@users.noreply.github.com>
2026-08-13 20:35:24 -0300

    Update Atividade1.1.php

--- Função/Atividade1.1.php
+++ Função/Atividade1.1.php
@@ -1,1 +1,20 @@
+<?php
+
+function fatorial(int $num1)
+{
+    $s = 1;
+    for ($i = 1; $i <= $num1; $i++) {
+        $s = $s * $i;
+    }
+    return $s;
+};
+
+
+for($i = 5; $i <= 12; $i++ ){
+
+echo fatorial($i) . "<br>";
+
+
+
+}
 
