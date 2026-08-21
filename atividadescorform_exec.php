e0648e1128b674410d3f70c0d835e20022710301
leodm5674 <98219987+leodm5674@users.noreply.github.com>
2026-08-20 21:57:04 -0300

    Create atividadescorform_exec.php

---
+++ atividadescorform_exec.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Selecione a Cor</title>
+</head>
+
+<body>
+    <h2>Cor</h2>
+    <form method="POST" action="corexec.php">
+        <select name="cor">
+            <option value="">--Selecione a Cor---</option>
+            <option value="Tomato">Tomato</option>
+            <option value="Orange">Orange</option>
+            <option value="DodgerBlue">DodgerBlue</option>
+        </select>
+        <br><br>
+        <button type="enviar">Enviar</button>
+    </form>
+</body>
+
+</html>
