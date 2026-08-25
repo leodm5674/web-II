cef2c3325009f98a7490922d7ec0709a9afa9184
leodm5674 <98219987+leodm5674@users.noreply.github.com>
2026-08-25 18:57:46 -0300

    Update Métodos GET e POST: progressão aritmética.php

--- atividadesava/Métodos GET e POST: progressão aritmética.php
+++ atividadesava/Métodos GET e POST: progressão aritmética.php
@@ -3,19 +3,19 @@
 
 
 
-if (!isset($_GET['inicio'])) {
+if (!isset($_GET['inicio']) || $_GET['inicio'] == "") {
     echo "Faltou inicio" . "<br>";
 }
 
-if (!isset($_GET['razao'])) {
+if (!isset($_GET['razao']) || $_GET['razao'] == "") {
     echo "Faltou razao" . "<br>";
 }
 
-if (!isset($_GET['quantidade'])) {
+if (!isset($_GET['quantidade']) || $_GET['quantidade'] == "") {
     echo "Faltou quantidade" . "<br>";
 }
 
-if (isset($_GET['inicio']) && isset($_GET['razao']) && isset($_GET["quantidade"])) {
+if (isset($_GET['inicio']) && $_GET['inicio'] != "" && isset($_GET['razao']) &&  $_GET['razao'] != "" && isset($_GET["quantidade"]) && $_GET['quantidade'] != "") {
     $inicio = $_GET["inicio"];
     $razao = $_GET["razao"];
     $quantidade = $_GET["quantidade"];
