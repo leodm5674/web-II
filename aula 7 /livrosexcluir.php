faa5d5488c2df16781bde25b195777ad05e2c5be
leodm5674 <98219987+leodm5674@users.noreply.github.com>
2026-08-27 22:04:00 -0300

    Create livrosexcluir.php

---
+++ aula 7 /livrosexcluir.php
@@ -0,0 +1,15 @@
+<?php
+
+//1 receber o id do livro
+
+// 2 carregar ou buscar os livros existentes no aruqivo json
+
+//3  encontrar o indice do livro a ser excluir
+
+
+//4 - executar a funcao para excluir
+//array_splice( , )
+
+//5 salvar os dados no arquivo jsons
+
+//6 redirecionar para o livros.php
