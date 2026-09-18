eab1fb7738401a214bb0820e6ff8bb566f6bb8d5
Leandro Dierings de Morais <98219987+leodm5674@users.noreply.github.com>
2026-09-17 21:44:25 -0300

    Create Pessoa class with name and surname methods
    Implement Pessoa class with name and surname properties, including getters and setters.

---
+++ Classes e Objetos/Pessoa/Pessoa.php
@@ -0,0 +1,58 @@
+<?php
+
+class Pessoa
+{
+
+
+    private $nome;
+    private $sobrenome;
+
+    public function retornanome()
+    {
+
+        $nomecompleto = $this->nome . " " . $this->sobrenome;
+        return $nomecompleto;
+    }
+
+
+
+
+
+
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome($nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sobrenome
+     */
+    public function getSobrenome()
+    {
+        return $this->sobrenome;
+    }
+
+    /**
+     * Set the value of sobrenome
+     */
+    public function setSobrenome($sobrenome): self
+    {
+        $this->sobrenome = $sobrenome;
+
+        return $this;
+    }
+}
